590a8702f1d22a211aba5b2404270d6574dcc3f1
chelseyseb <sbstnchlsy@gmail.com>
2023-04-23 14:08:31 +0800

    axios

--- src/components/forms-formik/index.js
+++ src/components/forms-formik/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useFormikContext } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 const styles = {
     container: {
         backgroundColor: "whitesmoke",
+        display: "flex",
+        flexDirection: "column",
         width: "98%",
         height: "100%",
         borderRadius: "5px",
@@ -91,44 +93,13 @@ const styles = {
     },
   }  
 
-  
-
-  // const patchPosts = (passValue) => {
-  //   console.log("passValue == ", passValue);
-  //   axios
-  //     .patch(`${api}/posts/1`, {
-  //       id	: passValue?.id,
-  //       firstName: passValue?.firstName,
-  //       lastName: passValue?.lastName,
-  //       emailNum: passValue?.emailNum,
-  //       password: passValue?.password,
-  //       month: passValue?.month,
-  //       day: passValue?.day,
-  //       year: passValue?.year,
-  //       gender: passValue?.gender,
-  //       pronoun: passValue?.pronoun,
-  //       customGender: passValue?.customGender,
-  //       // body: passValue?.body, --> include one key only for PUT
-  //     })
-  //     .then(function (response) {
-  //       setPatchStatus(response?.status);
-  //     });
-  // };
-
-  // const deletePosts = (passValue) => {
-  //   axios.delete(`${api}/posts/${passValue?.id}`).then(function (response) {
-  //     setDeleteStatus(response?.status);
-  //   });
-  // };
-
-
 const FormsFormik = () => {
     const api = "https://api.ahglab.com/api:W7k9W8HQ/users";
     const [data, setData] = useState();
     const [postStatus, setPostStatus] = useState();
     const [patchStatus, setPatchStatus] = useState();
     const [deleteStatus, setDeleteStatus] = useState();
-
+    const [display, setDisplay] = useState();
     
   const SignupSchema = Yup.object().shape({
     firstName: Yup.string()
@@ -140,7 +111,11 @@ const FormsFormik = () => {
       .max(70, "Too Long!")
       .required("Last Name is Required"),
     emailNum: Yup.string()
-        .required("Email is Required"),
+        .required("Email is Required")
+        .matches(
+          /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          "Invalid email address"
+        ),
     password: Yup.string()
         .min(8, "Too Short!")
         .max(50, "Too Long!")
@@ -175,7 +150,7 @@ const FormsFormik = () => {
     { value: "11", label: "November" },
     { value: "12", label: "December" },
   ];
-  
+
   const days = Array.from({ length: 31 }, (_, i) => {
     const day = i + 1;
     return {
@@ -188,12 +163,10 @@ const FormsFormik = () => {
     const year = new Date().getFullYear() - i;
     return { value: year.toString(), label: year.toString() };
   });
-  
 
-  const [display, setDisplay] = useState();
-      
     const getPosts = () => {
           axios.get(`${api}`).then(function (response) {
+            setData(response?.status);
             setData(response?.data);
           });
         };
@@ -207,49 +180,44 @@ const FormsFormik = () => {
             axios
               .post(`${api}`, {
                 id	: passValue?.id,
-                firstName: passValue?.firstName,
-                lastName: passValue?.lastName,
-                emailNum: passValue?.emailNum,
+                first_name: passValue?.firstName,
+                last_name: passValue?.lastName,
+                credentials: passValue?.emailNum,
                 password: passValue?.password,
-                month: passValue?.month,
-                day: passValue?.day,
-                year: passValue?.year,
+                birthdate: `${values?.month}/${values?.day}/${values?.year}`,
                 gender: passValue?.gender,
-                customGender: passValue?.customGender,
                 pronoun: passValue?.pronoun,
               })
               .then(function (response) {
                 setPostStatus(response?.status);
-                // console.log(response);
+                console.log(response);
               });
 
-                // const patchPosts = (passValue) => {
-                //   console.log("passValue == ", passValue);
-                //   axios
-                //     .patch(`${api}/posts/1`, {
-                //       id	: passValue?.id,
-                //       firstName: passValue?.firstName,
-                //       lastName: passValue?.lastName,
-                //       emailNum: passValue?.emailNum,
-                //       password: passValue?.password,
-                //       month: passValue?.month,
-                //       day: passValue?.day,
-                //       year: passValue?.year,
-                //       gender: passValue?.gender,
-                //       pronoun: passValue?.pronoun,
-                //       customGender: passValue?.customGender,
-                //       // body: passValue?.body, --> include one key only for PUT
-                //     })
-                //     .then(function (response) {
-                //       setPatchStatus(response?.status);
-                //     });
-                // };
+          };
 
-                // const deletePosts = (passValue) => {
-                //   axios.delete(`${api}/posts/${passValue?.id}`).then(function (response) {
-                //     setDeleteStatus(response?.status);
-                //   });
-                // };
+          const patchPosts = (passValue) => {
+            console.log("passValue == ", passValue);
+            axios
+              .put(`${api}/${passValue?.id}` ,{
+                id: passValue?.id,
+                first_name: passValue?.firstName,
+                last_name: passValue?.lastName,
+                credentials: passValue?.emailNum,
+                password: passValue?.password,
+                birthdate: passValue?.birthdate,
+                gender: passValue?.gender,
+                pronoun: passValue?.pronoun,
+                // body: passValue?.body, --> include one key only for PUT
+              })
+              .then(function (response) {
+                setPatchStatus(response?.status);
+              });
+          };
+
+          const deletePosts = (passValue) => {
+            axios.delete(`${api}/${passValue?.id}`).then(function (response) {
+              setDeleteStatus(response?.status);
+            });
           };
           
           return (
@@ -269,7 +237,7 @@ const FormsFormik = () => {
                 }}
                 validationSchema={SignupSchema}
                 onSubmit={(values, actions) => {
-                  postPosts(values);
+                  postPosts(values);                 
                   setDisplay(
                   `Name: ${values.firstName} ${values.lastName} Email/Number: ${values.emailNum} Password: ${values.password} Birthday: ${values.month}/${values.day}/${values.year} \n Gender: ${values.gender } ${values.pronoun} ${values.customGender}`
                     );
@@ -394,7 +362,7 @@ const FormsFormik = () => {
             <br></br>
             <p style={styles.p}>{display}</p>
             <p>
-                Status:{" "}
+                Post Status:{" "}
                 {postStatus === 200 ? (
                   <span style={{ color: "green" }}>Success</span>
                 ) : postStatus === 500 ? (
@@ -403,18 +371,215 @@ const FormsFormik = () => {
                   "N/A"
                 )}
               </p>
-              <p>GET</p>
-                {/* {data &&
-                  data.splice(0, 10).map((value, key) => {
-                    return (
-                      // <p>
-                      //   {key}: {value?.firstName}
-                      // </p>
-                    );
-                  })} */}
+
             </Form>
         )}
       </Formik>
+      <br></br>
+      <div>
+      <h3>EDIT</h3>
+      <Formik
+                initialValues={{
+                  id: "",
+                  firstName: "",
+                  lastName: "",
+                  emailNum: "",
+                  password: "",
+                  month: "",
+                  day: "",
+                  year: "",
+                  gender: "",
+                  pronoun: "",
+                  customGender: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={(values, actions) => {
+                  patchPosts(values);
+                  setDisplay(
+                  `Name: ${values.firstName} ${values.lastName} Email/Number: ${values.emailNum} Password: ${values.password} Birthday: ${values.month}/${values.day}/${values.year} \n Gender: ${values.gender } ${values.pronoun} ${values.customGender}`
+                    );
+                    actions.resetForm({
+                      values:{
+                        id: "",
+                        firstName: "",
+                        lastName: "",
+                        emailNum: "",
+                        password: "",
+                        month: "",
+                        day: "",
+                        year: "",
+                        gender: "",
+                        pronoun: "",
+                        customGender: "",
+                      },
+                    })
+                }}        
+              >
+        {({ errors, touched, values }) => (
+            <Form>
+              <Field style={styles.input} id="id" name="id" placeholder="ID number" />
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ width: "50%" }}>
+                <Field style={styles.input} id="firstName" name="firstName" placeholder="First Name" />
+                <ErrorMessage style={{ color: "red" }} name="firstName" />
+                </div>
+                <div style={{ width: "50%" }}>
+                    <Field style={styles.input} id="lastName" name="lastName" placeholder="Last Name" />
+                <ErrorMessage name="lastName" />
+            </div>  
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <Field style={styles.input} type="email" id="emailNum" name="emailNum" placeholder="Mobile number or email" />
+                <ErrorMessage name="emailNum" />
+                <Field style={styles.input} type="password" id="password" name="password" placeholder="New password" />
+                <ErrorMessage name="password" /> 
+
+            <label style={styles.label} htmlFor="birthday">Birthday </label>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ width: "33.33%" }}>
+                    <Field style={styles.option} as="select" name="month" id="month">
+                        <option  value="">Select month</option>
+                        {months.map((month) => (
+                        <option key={month.value} value={month.value}>
+                            {month.label}
+                        </option>
+                        ))}
+                    </Field>
+                    <ErrorMessage name="month" />
+                </div>
+
+            <div style={{ width: "33.33%" }}>
+                <Field style={styles.option}  as="select" name="day" id="day">
+                    <option value="">Select day</option>
+                    {days.map((day) => (
+                    <option key={day.value} value={day.value}>
+                        {day.label}
+                    </option>
+                    ))}
+                </Field>
+                <ErrorMessage name="day" />
+            </div>
+            <div style={{ width: "33.33%" }}>
+            <Field style={styles.option} as="select" name="year" id="year">
+                <option   value="">Select year</option>
+                {years.map((year) => (
+                <option key={year.value} value={year.value}>
+                    {year.label}
+                </option>
+                ))}
+            </Field>
+            <ErrorMessage name="year" />
+            </div>
+            </div>
+
+            <label style={styles.label} htmlFor="gender">Gender </label>
+            <div>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{ width: "33.33%" }}>
+                <label style={styles.radioLabel}  htmlFor="male">
+                Male
+                <Field style={styles.radio} type="radio" name="gender" id="male" value="male" />
+                </label>
+            </div>
+            
+            <div style={{ width: "33.33%" }}>
+            <label style={styles.radioLabel} htmlFor="female">
+                Female
+                <Field type="radio" name="gender" id="female" value="female" />
+                </label>
+            </div>
+            <div style={{ width: "33.33%" }}>
+            <label style={styles.radioLabel} htmlFor="custom">
+                Custom
+                <Field type="radio" name="gender" id="custom" value="custom" />
+                </label>
+                <ErrorMessage name="gender" />
+                </div>
+            </div>
+        </div>
+                {values?.gender == "custom" && (
+                <div>
+                    <Field style={styles.input} as="select" id="pronoun" name="pronoun" placeholder="Select your pronoun" >
+                    <option value="">Select your pronoun</option>
+                    {pronoun.map((pronoun) => (
+                    <option key={pronoun.value} value={pronoun.value}>
+                        {pronoun.label}
+                    </option>
+                    ))}
+                    </Field>
+                    <ErrorMessage name="pronoun" />
+                    <Field style={styles.input} id="customGender" name="customGender" placeholder="Gender (optional)" />
+                    
+                </div>
+                )}
+        </div>
+
+            <p style={styles.p}>People who use our service may have uploaded your contact information to Facebook. Learn more.</p>
+            <p style={styles.p2}>By clicking Sign Up, you agree to our Terms, Privacy Policy and Cookies Policy. You may receive SMS Notifications from us and can opt out any time</p>
+            <button style={styles.signUpButton} type="submit">Sign Up</button>
+            <br></br>
+            <p style={styles.p}>{display}</p>
+              <p>
+                Status:{" "}
+                {patchStatus === 200 ? (
+                  <span style={{ color: "green" }}>Success</span>
+                ) : patchStatus === 500 ? (
+                  <span style={{ color: "red" }}>Failed</span>
+                ) : (
+                  "N/A"
+                )}
+              </p>
+            </Form>
+            
+        )}
+      </Formik>
+      </div>
+
+      <div>
+        <br/>
+        <p>DELETE</p>
+        <Formik
+          initialValues={{
+            id: "",
+          }}
+          onSubmit={(values) => {
+            deletePosts(values);
+          }}
+        >
+          {({ errors, touched, values }) => (
+            <Form>
+              <label style={styles.label} htmlFor="id">ID </label>
+              <Field style={styles.input} id="id" name="id" placeholder="ID" />
+              <ErrorMessage style={{ color: "red" }} name="id" />
+              <br />
+              <button style={styles.signUpButton} type="submit">Delete</button>
+              <br/>
+              <p>
+                Delete Status:{" "}
+                {deleteStatus === 200 ? (
+                  <span style={{ color: "green" }}>Success</span>
+                ) : deleteStatus === 500 ? (
+                  <span style={{ color: "red" }}>Failed</span>
+                ) : (
+                  "N/A"
+                )}
+              </p>
+            </Form>
+          )}
+        </Formik>
+        <br/>
+        <div>
+        <p>GET</p>
+        {data && data.map((value, index) => {
+            return (
+              <p key={index}>
+                {index}: {value?.first_name}
+              </p>
+            );
+          })}
+      </div>
+      </div>
     </div>
   );
 };
